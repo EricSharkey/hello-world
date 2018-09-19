85a8719858fef736857473a94e2d47b5e9e2826e
Eric Sharkey <eric@lisaneric.org>
2018-09-19 19:52:50 -0400

    Slower shots.

--- aliens2.py
+++ aliens2.py
@@ -14,7 +14,7 @@ if not pygame.image.get_extended():
 #game constants
 MAX_SHOTS      = 2      #most player bullets onscreen
 ALIEN_ODDS     = 22     #chances a new alien appears
-BOMB_ODDS      = 10    #chances a new bomb will drop
+BOMB_ODDS      = 50    #chances a new bomb will drop
 ALIEN_RELOAD   = 12     #frames between new aliens
 SCREENRECT     = Rect(0, 0, 640, 480)
 SCORE          = 0
